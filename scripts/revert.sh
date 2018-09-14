9e5aafe7c4cefaba59b969e198599ef16b8ba808
mdali_moula <32926705+forpix@users.noreply.github.com>
2018-09-14 15:31:45 +0530

    Update revert.sh

--- scripts/revert.sh
+++ scripts/revert.sh
@@ -2,5 +2,5 @@
 a=$(git reflog|awk '{print $1;exit}')
 echo $a
 git revert $a
-git remote set-url origin http://forpix:mdali%40786@github.com/forpix/cherry-pick.git
+git remote set-url origin https://forpix:mdali%40786@github.com/forpix/cherry-pick.git
 git push -f origin master
