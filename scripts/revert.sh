59f33cb3c333aab0705b4d6ec8826fab78972694
mdali_moula <32926705+forpix@users.noreply.github.com>
2018-09-17 11:40:37 +0530

    Update revert.sh

--- scripts/revert.sh
+++ scripts/revert.sh
@@ -2,7 +2,7 @@
 a=$(git reflog|awk '{print $1;exit}')
 echo $a
 git revert $a
-
+git commit -m "commited for the reverting the changes"
 git remote set-url origin http://forpix:mdali%40786@github.com/forpix/cherry-pick.git
-git push -u origin working
+ git push origin HEAD:master
 
